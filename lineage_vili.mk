82ed072f971036351817c34aa337563612692137
eggyow <rvbeakzs@gmail.com>
2026-03-29 12:32:18 +0800

    Revert "vili: Derp-ify!"
    This reverts commit c928194e8feb8ec4610e4c10cc4f741e6c1a155a.

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -9,17 +9,6 @@ $(call inherit-product, device/xiaomi/vili/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# DerpFest Flags
-DERPFEST_BUILD_TYPE := Official
-WITH_GMS := true
-
-# Extras
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_ENABLE_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_INCLUDE_ACCORD := false
-TARGET_EXCLUDE_BACKUPTOOL := true
-
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
